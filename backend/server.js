1398529f31b3c178b8a887820f46f9ff5eb7c59b
piyushsingh24 <piyushsingh2706@gmail.com>
2025-09-15 01:26:04 +0530

    backend & frontend deploy update

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ connectDB()
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
